a59158214f2bc3dbbbaeb869f4007abba8ac5b8a
Vitor Barbarisi <vitorbarbarisi@gmail.com>
2025-07-01 16:10:37 -0300

    first commit

---
+++ rotiv.py
@@ -0,0 +1,74 @@
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import MemorySaver
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+llm = init_chat_model("openai:gpt-4.1")
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+memory = MemorySaver()
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile(checkpointer=memory)
+
+from IPython.display import Image, display
+
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+config = {"configurable": {"thread_id": "1"}}
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        # The config is the **second positional argument** to stream() or invoke()!
+        events = graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]},
+            config,
+            stream_mode="values",
+        )
+        for event in events:
+            event["messages"][-1].pretty_print()
+        # stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
